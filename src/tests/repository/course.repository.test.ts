4e5d05f4419c8d59902ee382ece70037bfeebc2e
faleialina <august3@mail.ru>
2023-07-30 18:38:40 +0300

    course.repository.test

---
+++ src/tests/repository/course.repository.test.ts
@@ -0,0 +1,68 @@
+import { getAllCourseDB, getByIdCourseDB, createCourseDB, updateCourseDB, deleteCourseDB } from '../../repository/course.repository';
+
+const client = { query: jest.fn() };
+
+jest.mock('pg', () => {
+    const pool = { connect: jest.fn(() => client) };
+    return { Pool: jest.fn(() => pool) }
+});
+describe('getAllCourseDB:', () => {
+    test('test1', async () => {
+        client.query.mockResolvedValue({ rows: [{ id: '1', course: 'js' }] });
+        const result = await getAllCourseDB();
+        expect(client.query).toHaveBeenCalled();
+        expect(result).toEqual([{ id: '1', course: 'js' }]);
+        expect(result[0].id).toBe('1');
+        expect(result[0].course).toBe('js');
+        expect(result.length).toBe(1);
+        expect(result).toHaveLength(1);
+    });
+});
+describe('getByIdCourseDB:', () => {
+    test('test1', async () => {
+        client.query.mockResolvedValue({ rows: [{ id: '1', course: 'js' }] });
+        const result = await getByIdCourseDB('1');
+        expect(client.query).toHaveBeenCalled();
+        expect(result).toEqual([{ id: '1', course: 'js' }]);
+        expect(result[0].id).toBe('1');
+        expect(result[0].course).toBe('js');
+        expect(result.length).toBe(1);
+        expect(result).toHaveLength(1);
+    });
+});
+describe('createCourseDB:', () => {
+    test('test1', async () => {
+        client.query.mockResolvedValue({ rows: [{ id: '1', course: 'js' }] });
+        const result = await createCourseDB('js');
+        expect(client.query).toHaveBeenCalled();
+        expect(result).toEqual([{ id: '1', course: 'js' }]);
+        expect(result[0].id).toBe('1');
+        expect(result[0].course).toBe('js');
+        expect(result.length).toBe(1);
+        expect(result).toHaveLength(1);
+    });
+});
+describe('updateCourseDB:', () => {
+    test('test1', async () => {
+        client.query.mockResolvedValue({ rows: [{ id: '1', course: 'js' }] });
+        const result = await updateCourseDB('1','js');
+        expect(client.query).toHaveBeenCalled();
+        expect(result).toEqual([{ id: '1', course: 'js' }]);
+        expect(result[0].id).toBe('1');
+        expect(result[0].course).toBe('js');
+        expect(result.length).toBe(1);
+        expect(result).toHaveLength(1);
+    });
+});
+describe('deleteCourseDB:', () => {
+    test('test1', async () => {
+        client.query.mockResolvedValue({ rows: [{ id: '1', course: 'js' }] });
+        const result = await deleteCourseDB('1');
+        expect(client.query).toHaveBeenCalled();
+        expect(result).toEqual([{ id: '1', course: 'js' }]);
+        expect(result[0].id).toBe('1');
+        expect(result[0].course).toBe('js');
+        expect(result.length).toBe(1);
+        expect(result).toHaveLength(1);
+    });
+});
