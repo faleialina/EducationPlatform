1c1c076b58ef2508ff792dae782ef966f959bf5c
faleialina <august3@mail.ru>
2023-08-06 16:28:37 +0300

    client

---
+++ client/src/pages/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import Header from "../../companents/Header/Header";
+import Preview from "../../companents/Preview/Preview";
+
+
+function HomePage() {
+    return (
+        <div>
+            <Header />
+            <Preview />
+        </div>
+    )
+};
+
+export default HomePage;
