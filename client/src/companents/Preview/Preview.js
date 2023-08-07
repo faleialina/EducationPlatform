65851fa68df00855a38167ea2d3f47e1276dd0cb
faleialina <august3@mail.ru>
2023-08-07 23:41:14 +0300

    client/platforma

--- client/src/companents/Preview/Preview.js
+++ client/src/companents/Preview/Preview.js
@@ -8,7 +8,19 @@ function Preview() {
                     teaching online,
                     made easy.</h1>
                 <p className={style.textInfo}>Any subject, in any language, on any device, for all ages!</p>
+
+                <div className={style.platforma}>
+                    <div className={style.aboutPlatform}>About platform</div>
+                    <div className={style.previewStudents}>
+                        <div className={style.zipper}></div>
+                        <p>300</p>
+                        <p>+</p>
+                    </div>
+                    <p>Students</p>
+                </div>
             </div>
+
+
 
             <div className={style.previewImg}></div>
         </div>
