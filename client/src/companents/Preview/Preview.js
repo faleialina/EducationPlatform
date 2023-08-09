e663331046ff2818f32b3a8c39ea521a367eaa62
faleialina <august3@mail.ru>
2023-08-09 21:27:37 +0300

    client/learn/knowledge/progress

--- client/src/companents/Preview/Preview.js
+++ client/src/companents/Preview/Preview.js
@@ -1,28 +1,58 @@
 import style from './style.module.css';
 function Preview() {
     return (
-        <div className={style.preview}>
-            <div className={style.previewInfo}>
-                <p className={style.previewAbout}>E-COURSE PLATFORM</p>
-                <h1>Learning and
-                    teaching online,
-                    made easy.</h1>
-                <p className={style.textInfo}>Any subject, in any language, on any device, for all ages!</p>
+        <div >
+            <div className={style.preview}>
+                <div className={style.previewInfo}>
+                    <p className={style.previewAbout}>E-COURSE PLATFORM</p>
+                    <h1>Learning and
+                        teaching online,
+                        made easy.</h1>
+                    <p className={style.textInfo}>Any subject, in any language, on any device, for all ages!</p>
 
-                <div className={style.platforma}>
-                    <div className={style.aboutPlatform}>About platform</div>
-                    <div className={style.previewStudents}>
-                        <div className={style.zipper}></div>
-                        <p>600</p>
-                        <p className={style.plus}>+</p>
+                    <div className={style.platforma}>
+                        <div className={style.aboutPlatform}>About platform</div>
+                        <div className={style.previewStudents}>
+                            <div className={style.zipper}></div>
+                            <p>600</p>
+                            <p className={style.plus}>+</p>
+                        </div>
+                        <p>Students</p>
                     </div>
-                    <p>Students</p>
+                </div>
+                <div className={style.previewImg}></div>
+            </div>
+
+            <div className={style.learn}>
+                <div className={style.img}></div>
+                <div className={style.info}>
+                    <h1>Learn a language in a playful way</h1>
+                    <p>Make learning programming languages more fun with mini-games</p>
+                    <div className={style.icons}>
+                        <div className={style.icon1}></div>
+                        <div className={style.icon2}></div>
+                    </div>
                 </div>
             </div>
 
+            <div className={style.knowledge}>
+                <div className={style.info}>
+                    <h1>Increase your knowledge</h1>
+                    <p>Traditional and new effective approaches to learning languages</p>
+                    <div className={style.btnKnow}>Textbook →</div>
+                </div>
+                <div className={style.img}></div>
+            </div>
 
+            <div className={style.progress}>
+                <div className={style.img}></div>
+                <div className={style.info}>
+                    <h1>Watch your progress every day</h1>
+                    <p>Save statistics on your achievements and mistakes</p>
+                    <div className={style.btnKnow}>Statistics →</div>
+                </div>
+            </div>
 
-            <div className={style.previewImg}></div>
         </div>
     )
 };
